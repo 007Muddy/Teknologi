0ed94a0e9b5e8e13abccaee24d3ab3350a9736d0
007Muddy <101377307+007Muddy@users.noreply.github.com>
2023-09-21 13:01:53 +0200

    1
    1

---
+++ auktionshus/Server/Model/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Model
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+
+        public decimal MinPrice { get; set; }
+        public decimal SalePrice { get; set; }
+
+        public int AuktionTime { get; set; }
+
+        public DateTime EndTime
+        {
+            get
+            {
+                return DateTime.Now.AddSeconds(AuktionTime*60);
+            }
+
+        }
+
+        public Product(int id, Decimal minPrice, Decimal salePrice)
+        
+        {
+        Id = id;
+            MinPrice = minPrice;    
+            SalePrice = salePrice;  
+        
+        }
+
+    }
+}
